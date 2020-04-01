b8ad132602241b9d65890900007f529060f9f2b8
zacharymnp <56311867+zacharymnp@users.noreply.github.com>
2020-04-01 18:53:23 -0400

    slightly optimized doMath()

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,5 +1,3 @@
-package calculator;
-
 import java.awt.Color;
 import java.awt.Font;
 import java.awt.Graphics;
@@ -65,146 +63,139 @@ public class Calculator extends JPanel implements ActionListener {
 	 * @param a The text in the display
 	 * @return String The text to be output
 	 */
-	public String doMath(String s) {
+	public void doMath(String s) {
+		//TODO remove error where there is an tiny extra decimal
+		//TODO add additional functions
+		//TODO add PEMDAS
+		//TODO look into BigDecimal to add precision
 		List<Double> nums = new ArrayList<Double>();
 		List<String> operators = new ArrayList<String>();
-		double currentNum = 0;
-		Double x = 0d;
+		Double num = 0d;
 		String output = "undefined";
 		int decimalPlaces = 0;
 		
+		//for loop adds numbers and operators in the String to their respective Lists
 		for (int i = 0; i < s.length(); i++) {
-			if (s.substring(i,i+1).equals("0")) { 
+			if (s.charAt(i) == '0') { 
 				if (decimalPlaces == 0) {
-					x *= 10.0;
+					num *= 10.0;
 				}
 				else {
 					decimalPlaces++;
 				}
 			}
-			else if (s.substring(i,i+1).equals("1")) {
+			else if (s.charAt(i) == '1') {
 				if (decimalPlaces == 0) {
-					x *= 10.0; x += 1.0;
+					num *= 10.0; num += 1.0;
 				}
 				else {
-					x += 1.0/(Math.pow(10, decimalPlaces));
+					num += 1.0/(Math.pow(10, decimalPlaces));
 					decimalPlaces++;
 				}
 			}
-			else if (s.substring(i,i+1).equals("2")) { 
+			else if (s.charAt(i) == '2') { 
 				if (decimalPlaces == 0) {
-					x *= 10.0; x += 2.0;
+					num *= 10.0; num += 2.0;
 				}
 				else {
-					x += 2.0/(Math.pow(10, decimalPlaces));
+					num += 2.0/(Math.pow(10, decimalPlaces));
 					decimalPlaces++;
 				}
 			}
-			else if (s.substring(i,i+1).equals("3")) { 
+			else if (s.charAt(i) == '3') { 
 				if (decimalPlaces == 0) {
-					x *= 10.0; x += 3.0;
+					num *= 10.0; num += 3.0;
 				}
 				else {
-					x += 3.0/(Math.pow(10, decimalPlaces));
+					num += 3.0/(Math.pow(10, decimalPlaces));
 					decimalPlaces++;
 				}
 			}
-			else if (s.substring(i,i+1).equals("4")) { 
+			else if (s.charAt(i) == '4') { 
 				if (decimalPlaces == 0) {
-					x *= 10.0; x += 4.0;
+					num *= 10.0; num += 4.0;
 				}
 				else {
-					x += 4.0/(Math.pow(10, decimalPlaces));
+					num += 4.0/(Math.pow(10, decimalPlaces));
 					decimalPlaces++;
 				}
 			}
-			else if (s.substring(i,i+1).equals("5")) { 
+			else if (s.charAt(i) == '5') { 
 				if (decimalPlaces == 0) {
-					x *= 10.0; x += 5.0;
+					num *= 10.0; num += 5.0;
 				}
 				else {
-					x += 5.0/(Math.pow(10, decimalPlaces));
+					num += 5.0/(Math.pow(10, decimalPlaces));
 					decimalPlaces++;
 				}
 			}
-			else if (s.substring(i,i+1).equals("6")) { 
+			else if (s.charAt(i) == '6') { 
 				if (decimalPlaces == 0) {
-					x *= 10.0; x += 6.0;
+					num *= 10.0; num += 6.0;
 				}
 				else {
-					x += 6.0/(Math.pow(10, decimalPlaces));
+					num += 6.0/(Math.pow(10, decimalPlaces));
 					decimalPlaces++;
 				}
 			}
-			else if (s.substring(i,i+1).equals("7")) { 
+			else if (s.charAt(i) == '7') { 
 				if (decimalPlaces == 0) {
-					x *= 10.0; x += 7.0;
+					num *= 10.0; num += 7.0;
 				}
 				else {
-					x += 7.0/(Math.pow(10, decimalPlaces));
+					num += 7.0/(Math.pow(10, decimalPlaces));
 					decimalPlaces++;
 				}
 			}
-			else if (s.substring(i,i+1).equals("8")) { 
+			else if (s.charAt(i) == '8') { 
 				if (decimalPlaces == 0) {
-					x *= 10.0; x += 8.0;
+					num *= 10.0; num += 8.0;
 				}
 				else {
-					x += 8.0/(Math.pow(10, decimalPlaces));
+					num += 8.0/(Math.pow(10, decimalPlaces));
 					decimalPlaces++;
 				}
 			}
-			else if (s.substring(i,i+1).equals("9")) { 
+			else if (s.charAt(i) == '9') { 
 				if (decimalPlaces == 0) {
-					x *= 10.0; x += 9.0;
+					num *= 10.0; num += 9.0;
 				}
 				else {
-					x += 9.0/(Math.pow(10, decimalPlaces));
+					num += 9.0/(Math.pow(10, decimalPlaces));
 					decimalPlaces++;
 				}
 			}
-			//TODO make decimals work
-			else if (s.substring(i,i+1).equals(".")) { decimalPlaces = 1; }
-			else if (s.substring(i,i+1).equals("+")) { operators.add("+"); nums.add(x); x = 0.0; decimalPlaces = 0; }
-			else if (s.substring(i,i+1).equals("-")) { operators.add("-"); nums.add(x); x = 0.0; decimalPlaces = 0;}
-			else if (s.substring(i,i+1).equals("*")) { operators.add("*"); nums.add(x); x = 0.0; decimalPlaces = 0;}
-			else if (s.substring(i,i+1).equals("/")) { operators.add("/"); nums.add(x); x = 0.0; decimalPlaces = 0;}
-			//TODO add additional functions
+			
+			else if (s.charAt(i) == '.') { decimalPlaces = 1; }
+			else if (s.charAt(i) == '+') { operators.add("+"); nums.add(num); num = 0.0; decimalPlaces = 0;}
+			else if (s.charAt(i) == '-') { operators.add("-"); nums.add(num); num = 0.0; decimalPlaces = 0;}
+			else if (s.charAt(i) == '*') { operators.add("*"); nums.add(num); num = 0.0; decimalPlaces = 0;}
+			else if (s.charAt(i) == '/') { operators.add("/"); nums.add(num); num = 0.0; decimalPlaces = 0;}
 		}
 		
-		nums.add(x);
-		x = 0.0;
+		nums.add(num); //adding the last number to nums
+		num = 0.0;
 		
-		//TODO optimize PEMDAS
+		//for loop actually does the math
 		for (int i = 0; i < operators.size(); i++) {
 			if (operators.get(i).equals("*")) {
-				currentNum = nums.get(0) * nums.get(i+1);
-				nums.set(0,currentNum);
+				nums.set(0, nums.get(0) * nums.get(i + 1));
 			}
-			
 			else if (operators.get(i).equals("/")) {
-				currentNum = nums.get(0) / nums.get(i+1);
-				nums.set(0,currentNum);
+				nums.set(0, nums.get(0) / nums.get(i + 1));
 			}
-		
 			else if (operators.get(i).equals("+")) {
-				currentNum = nums.get(0) + nums.get(i+1);
-				nums.set(0,currentNum);
+				nums.set(0, nums.get(0) + nums.get(i + 1));
 			}
-			
-
 			else if (operators.get(i).equals("-")) {
-				currentNum = nums.get(0) - nums.get(i+1);
-				nums.set(0,currentNum);
+				nums.set(0, nums.get(0) - nums.get(i + 1));
 			}
 		}
-		currentNum = nums.get(0);
-		output = String.valueOf(currentNum);
+		nums.set(0, round(nums.get(0)));
+		output = String.valueOf(nums.get(0));
 		
-		System.out.println(output);
-		display = output;
-		return output;
-		
+		System.out.println(output); //putting output in console for debugging purposes
+		display = output; //putting output on display		
 	}
 	
 	/**
