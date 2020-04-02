0a766aea043b6e2513fb7da529069795926011fc
zacharymnp <56311867+zacharymnp@users.noreply.github.com>
2020-04-01 20:00:58 -0400

    Update CalculatorMath.java

--- src/thePackage/CalculatorMath.java
+++ src/thePackage/CalculatorMath.java
@@ -1,16 +1,7 @@
 package thePackage;
 
-import java.awt.Color;
-import java.awt.Font;
-import java.awt.Graphics;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import java.util.ArrayList;
 import java.util.List;
-
-import javax.swing.JButton;
-import javax.swing.JFrame;
-import javax.swing.JPanel;
 
 /**
  * <h1>CalculatorMath<h1>
@@ -22,50 +13,14 @@ import javax.swing.JPanel;
  * @author Maya
  * @since 2019-12-13
  */
-public class CalculatorMath extends JPanel implements ActionListener {
-	private static final long serialVersionUID = 1L;
-	
-	public static String display = "";
-	public static boolean isOnAdditionalButtons = false;
-	
-	public static ArrayList<String> terms = new ArrayList<String>(); //figure out how to differentiate between terms
-	
-	public static JButton equalsButton = new JButton("=");
-	public static JButton deleteButton = new JButton("DEL");
-	public static JButton oneButton = new JButton("1");
-	public static JButton twoButton = new JButton("2");
-	public static JButton threeButton = new JButton("3");
-	public static JButton fourButton = new JButton("4");
-	public static JButton fiveButton = new JButton("5");
-	public static JButton sixButton = new JButton("6");
-	public static JButton sevenButton = new JButton("7");
-	public static JButton eightButton = new JButton("8");
-	public static JButton nineButton = new JButton("9");
-	public static JButton zeroButton = new JButton("0");
-	public static JButton pointButton = new JButton(".");
-	public static JButton plusButton = new JButton("+");
-	public static JButton minusButton = new JButton("-");
-	public static JButton timesButton = new JButton("*");
-	public static JButton dividedByButton = new JButton("/");
-	public static JButton additionalFunctionsButton = new JButton("Additional Functions");
-	public static JButton leftParenthesisButton = new JButton("(");
-	public static JButton rightParenthesisButton = new JButton(")");
-	public static JButton sineButton = new JButton("sin");
-	public static JButton cosineButton = new JButton("cos");
-	public static JButton tangentButton = new JButton("tan");
-	public static JButton factorialButton = new JButton("!");
-	public static JButton logButton = new JButton("log");
-	public static JButton naturalLogButton = new JButton("ln");
-	public static JButton exponentButton = new JButton("^");
-	public static JButton squareRootButton = new JButton("^.5");
-	
+public class CalculatorMath {	
 
 	/**
 	 * This method is run when the "equal" button is pressed and decides what math to do
 	 * @param a The text in the display
 	 * @return String The text to be output
 	 */
-	public void doMath(String s) {
+	public static void doMath(String s) {
 		//TODO remove error where there is an tiny extra decimal
 		//TODO add additional functions
 		//TODO add PEMDAS
@@ -193,11 +148,11 @@ public class CalculatorMath extends JPanel implements ActionListener {
 				nums.set(0, nums.get(0) - nums.get(i + 1));
 			}
 		}
-		nums.set(0, round(nums.get(0)));
+		nums.set(0, nums.get(0));
 		output = String.valueOf(nums.get(0));
 		
 		System.out.println(output); //putting output in console for debugging purposes
-		display = output; //putting output on display		
+		CalculatorGUI.display = output; //putting output on display		
 	}
 	
 	/**
@@ -207,198 +162,5 @@ public class CalculatorMath extends JPanel implements ActionListener {
 	 */	
 	public double round(double a) {
 		return ((double)((int)(a * 1000)) / 1000);
-	}
-	
-	/**
-	 * This method is used to add two doubles together
-	 * @param a The number to be added to
-	 * @param b The number to be added by
-	 * @return double The sum of the two parameters
-	 */
-	public static void showAdditionalFunctions() {
-		if (isOnAdditionalButtons == false) {
-			zeroButton.setVisible(false);
-			oneButton.setVisible(false);
-			twoButton.setVisible(false);
-			threeButton.setVisible(false);
-			fourButton.setVisible(false);
-			fiveButton.setVisible(false);
-			sixButton.setVisible(false);
-			sevenButton.setVisible(false);
-			eightButton.setVisible(false);
-			nineButton.setVisible(false);
-			leftParenthesisButton.setVisible(true);
-			rightParenthesisButton.setVisible(true);
-			sineButton.setVisible(true);
-			cosineButton.setVisible(true);
-			tangentButton.setVisible(true);
-			factorialButton.setVisible(true);
-			logButton.setVisible(true);
-			naturalLogButton.setVisible(true);
-			exponentButton.setVisible(true);
-			squareRootButton.setVisible(true);
-			isOnAdditionalButtons = true;
-		}
-		else {
-			leftParenthesisButton.setVisible(false);
-			rightParenthesisButton.setVisible(false);
-			sineButton.setVisible(false);
-			cosineButton.setVisible(false);
-			tangentButton.setVisible(false);
-			factorialButton.setVisible(false);
-			logButton.setVisible(false);
-			naturalLogButton.setVisible(false);
-			exponentButton.setVisible(false);
-			squareRootButton.setVisible(false);
-			zeroButton.setVisible(true);
-			oneButton.setVisible(true);
-			twoButton.setVisible(true);
-			threeButton.setVisible(true);
-			fourButton.setVisible(true);
-			fiveButton.setVisible(true);
-			sixButton.setVisible(true);
-			sevenButton.setVisible(true);
-			eightButton.setVisible(true);
-			nineButton.setVisible(true);
-			isOnAdditionalButtons = false;
-
-		}
-	}
-	
-	@Override
-	public void paint(Graphics g) {
-		super.paint(g);
-		g.setColor(Color.black);
-		g.setFont(new Font("TimesRoman", Font.PLAIN, 40));
-		g.drawString(display, 10, 100);
-	}
-	
-	@Override
-	public void actionPerformed(ActionEvent e) {
-		JButton clickedButton = (JButton)(e.getSource());
-		System.out.println(clickedButton.getText());
-		if (clickedButton.getText() == "DEL") {
-			if (display.equals("Go away")) {
-				display = "";
-			}
-			else if (display.length() != 0 ) {
-				display = display.substring(0, display.length() - 1);
-			}
-		}
-		else if (clickedButton.getText() == "=") {
-			doMath(display);
-		}
-		else if (clickedButton.getText() == "Additional Functions") {
-			showAdditionalFunctions();
-		}
-		else if (display.length() < 14) {
-			display = display.concat(clickedButton.getText());
-		}
-		else {
-			display = "Go away";
-		}
-		repaint();
-	}
-	
-	/**
-	 * This is the main method, which creates CalculatorMathFrame and adds in the buttons
-	 * @param args
-	 * @return Nothing
-	 */
-	public static void main(String[] args) {
-		
-	CalculatorMath CalculatorMath = new CalculatorMath();
-	JFrame CalculatorMathFrame = new JFrame();
-	CalculatorMathFrame.getContentPane().add(CalculatorMath);
-	CalculatorMathFrame.setSize(416, 599);
-	CalculatorMathFrame.setVisible(true);
-	CalculatorMathFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-	CalculatorMathFrame.setResizable(false);
-	CalculatorMath.setLayout(null); //we may want to use a layout, so that we don't need to worry about absolute positioning
-	
-	equalsButton.addActionListener(CalculatorMath);
-	CalculatorMath.add(equalsButton);
-	equalsButton.setBounds(200, 450, 100, 110);
-	deleteButton.addActionListener(CalculatorMath);
-	CalculatorMath.add(deleteButton);
-	deleteButton.setBounds(300, 50, 100, 70);
-	oneButton.addActionListener(CalculatorMath);
-	CalculatorMath.add(oneButton);
-	oneButton.setBounds(0, 340, 100, 110);
-	twoButton.addActionListener(CalculatorMath);
-	CalculatorMath.add(twoButton);
-	twoButton.setBounds(100, 340, 100, 110);
-	threeButton.addActionListener(CalculatorMath);
-	CalculatorMath.add(threeButton);
-	threeButton.setBounds(200, 340, 100, 110);
-	fourButton.addActionListener(CalculatorMath);
-	CalculatorMath.add(fourButton);
-	fourButton.setBounds(0, 230, 100, 110);
-	fiveButton.addActionListener(CalculatorMath);
-	CalculatorMath.add(fiveButton);
-	fiveButton.setBounds(100, 230, 100, 110);
-	sixButton.addActionListener(CalculatorMath);
-	CalculatorMath.add(sixButton);
-	sixButton.setBounds(200, 230, 100, 110);
-	sevenButton.addActionListener(CalculatorMath);
-	CalculatorMath.add(sevenButton);
-	sevenButton.setBounds(0, 120, 100, 110);
-	eightButton.addActionListener(CalculatorMath);
-	CalculatorMath.add(eightButton);
-	eightButton.setBounds(100, 120, 100, 110);
-	nineButton.addActionListener(CalculatorMath);
-	CalculatorMath.add(nineButton);
-	nineButton.setBounds(200, 120, 100, 110);
-	zeroButton.addActionListener(CalculatorMath);
-	CalculatorMath.add(zeroButton);
-	zeroButton.setBounds(100, 450, 100, 110);
-	pointButton.addActionListener(CalculatorMath);
-	CalculatorMath.add(pointButton);
-	pointButton.setBounds(0, 450, 100, 110);
-	plusButton.addActionListener(CalculatorMath);
-	CalculatorMath.add(plusButton);
-	plusButton.setBounds(300, 450, 100, 110);
-	minusButton.addActionListener(CalculatorMath);
-	CalculatorMath.add(minusButton);
-	minusButton.setBounds(300, 340, 100, 110);
-	timesButton.addActionListener(CalculatorMath);
-	CalculatorMath.add(timesButton);
-	timesButton.setBounds(300, 230, 100, 110);
-	dividedByButton.addActionListener(CalculatorMath);
-	CalculatorMath.add(dividedByButton);
-	dividedByButton.setBounds(300, 120, 100, 110);
-	additionalFunctionsButton.addActionListener(CalculatorMath);
-	CalculatorMath.add(additionalFunctionsButton);
-	additionalFunctionsButton.setBounds(0, 0, 416, 50);
-	leftParenthesisButton.addActionListener(CalculatorMath);
-	CalculatorMath.add(leftParenthesisButton);
-	leftParenthesisButton.setBounds(0, 340, 100, 110);
-	rightParenthesisButton.addActionListener(CalculatorMath);
-	CalculatorMath.add(rightParenthesisButton);
-	rightParenthesisButton.setBounds(100, 340, 100, 110);
-	exponentButton.addActionListener(CalculatorMath);
-	CalculatorMath.add(exponentButton);
-	exponentButton.setBounds(200, 340, 100, 110);
-	squareRootButton.addActionListener(CalculatorMath);
-	CalculatorMath.add(squareRootButton);
-	squareRootButton.setBounds(100, 450, 100, 110);
-	naturalLogButton.addActionListener(CalculatorMath);
-	CalculatorMath.add(naturalLogButton);
-	naturalLogButton.setBounds(100, 230, 100, 110);
-	logButton.addActionListener(CalculatorMath);
-	CalculatorMath.add(logButton);
-	logButton.setBounds(0, 230, 100, 110);
-	factorialButton.addActionListener(CalculatorMath);
-	CalculatorMath.add(factorialButton);
-	factorialButton.setBounds(200, 230, 100, 110);
-	sineButton.addActionListener(CalculatorMath);
-	CalculatorMath.add(sineButton);
-	sineButton.setBounds(0, 120, 100, 110);
-	cosineButton.addActionListener(CalculatorMath);
-	CalculatorMath.add(cosineButton);
-	cosineButton.setBounds(100, 120, 100, 110);
-	tangentButton.addActionListener(CalculatorMath);
-	CalculatorMath.add(tangentButton);
-	tangentButton.setBounds(200, 120, 100, 110);
 	}
 }
